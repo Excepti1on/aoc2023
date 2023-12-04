e760ac955f60306fefa36e5b6febbb18f49f0f3f
Oliver Wölbing <oliver.woelbing@gmail.com>
2023-12-04 11:49:39 +0100

    refactor day 4

--- day4/d4.c
+++ day4/d4.c
@@ -13,57 +13,51 @@
 #define CHECK_CARDS 25
 #define CARD_COUNT 198
 
-typedef struct Card {
-	uint64_t count;
-	uint64_t winning_cards[WINNING_CARDS];
-	uint64_t checking_cards[CHECK_CARDS];
-} Card;
-
-uint64_t Evaluate(Card **cards, size_t index) {
-	uint64_t sum = 1;
-	for (size_t i = 0; i < cards[index]->count; ++i) {
-		sum += Evaluate(cards, index + i + 1);
+uint32_t Evaluate(size_t n, uint32_t counts[static n], size_t index) {
+	uint32_t sum = 1;
+	for (size_t i = 0; i < counts[index]; ++i) {
+		sum += Evaluate(n, counts, index + i + 1);
 	}
 	return sum;
 }
 
 void Day4() {
 	FILE *file = fopen("../day4/input.txt", "r");
+
 	char buffer[128] = {};
-	uint64_t global_sum = 0;
-	Card **cards = malloc(CARD_COUNT * sizeof(Card *));
+	uint32_t counts[CARD_COUNT] = {};
+	uint32_t winning_cards[WINNING_CARDS] = {};
+	uint32_t checking_cards[CHECK_CARDS] = {};
 	size_t index = 0;
 	while (fgets(buffer, sizeof buffer, file) != NULL) {
 		strtok(buffer, ":");
-		Card *card = malloc(sizeof(Card));
 		char *p = strtok(NULL, " ");
 		for (size_t i = 0; i < WINNING_CARDS; ++i, p = strtok(NULL, " | ")) {
-			card->winning_cards[i] = strtoull(p, NULL, 10);
+			winning_cards[i] = strtoul(p, NULL, 10);
 		}
-		for (size_t i = 0; i < CHECK_CARDS; ++i, p = strtok(NULL, " | \n")) {
-			card->checking_cards[i] = strtoull(p, NULL, 10);
+		for (size_t i = 0; i < CHECK_CARDS; ++i, p = strtok(NULL, " \n")) {
+			checking_cards[i] = strtoul(p, NULL, 10);
 		}
-		card->count = 0;
+		counts[index] = 0;
 		for (size_t i = 0; i < CHECK_CARDS; ++i) {
 			for (size_t j = 0; j < WINNING_CARDS; ++j) {
-				if (card->winning_cards[j] == card->checking_cards[i]) {
-					card->count++;
+				if (winning_cards[j] == checking_cards[i]) {
+					counts[index]++;
 				}
 			}
 		}
-		cards[index] = card;
 		index++;
 	}
 	fclose(file);
-	for (size_t i = 0; i < CARD_COUNT; ++i) {
-		global_sum += (cards[i]->count > 0) ? 1 << (cards[i]->count - 1) : 0;
+	uint32_t global_sum = 0;
+	for (size_t i = 0; i < index; ++i) {
+		global_sum += (counts[i] > 0) ? 1 << (counts[i] - 1) : 0;
 	}
-	printf("Points Part 1: %llu\n", global_sum);
+	printf("Points Part 1: %u\n", global_sum);
+
 	global_sum = 0;
-	for (size_t i = 0; i < CARD_COUNT; ++i) {
-		global_sum += Evaluate(cards, i);
-		free(cards[i]);
+	for (size_t i = 0; i < index; ++i) {
+		global_sum += Evaluate(index, counts, i);
 	}
-	free(cards);
-	printf("Cards Part 2: %llu\n", global_sum);
+	printf("Cards Part 2: %u\n", global_sum);
 }
