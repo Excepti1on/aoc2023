cdd4f03a008cf5d766b3025b77758988ceb6be61
Oliver Woelbing <oliver.woelbing@gmail.com>
2023-12-13 00:01:51 +0100

    added faster better hashmap

--- day12/d12.c
+++ day12/d12.c
@@ -3,6 +3,7 @@
 //
 
 #include "d12.h"
+#include "../hashmap/map.h"
 
 #include <stdio.h>
 #include <search.h>
@@ -11,20 +12,23 @@
 #include <stdint.h>
 #include <limits.h>
 
+hashmap* m;
+
 #define max(a,b) \
    ({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
      _a > _b ? _a : _b; })
 
-static uint64_t Solve(int str_len, char str[static str_len], int key_len, int keys[static key_len]);
+static uint64_t Solve(int str_len, char str[static str_len], int key_len, char keys[static key_len]);
 
-static uint64_t FoundHash(int str_len, char str[static str_len], int key_len, int keys[static key_len]);
+static uint64_t FoundHash(int str_len, char str[static str_len], int key_len, char keys[static key_len]);
 
 void Day12() {
 	FILE *file = fopen("../day12/input.txt", "r");
 	char buffer[256];
 	uint64_t sum = 0;
 	uint64_t sum2 = 0;
+	m = hashmap_create();
 	while (fgets(buffer, sizeof buffer, file) != NULL) {
 		char *input = strtok(buffer, " ");
 		char *keys = strtok(NULL, "/n");
@@ -32,24 +36,22 @@ void Day12() {
 		char *temp = keys;
 		int key_len;
 		for (key_len = 0; temp[key_len]; temp[key_len] == ',' ? key_len++ : *temp++);
-		int keys_i[key_len];
-		keys_i[0] = (int) strtol(keys, &keys, 10);
+		uint8_t keys_i[key_len] = {};
+		keys_i[0] = (uint8_t)strtol(keys, &keys, 10);
 		for (int i = 0; i < key_len; ++i) {
-			keys_i[i + 1] = (int) strtol(keys + 1, &keys, 10);
+			keys_i[i + 1] = (char)strtol(keys + 1, &keys, 10);
 		}
 		key_len++;
-		int keys2[key_len * 5] = {};
+		char keys2[key_len * 5];
 		for (int i = 0; i < key_len * 5; ++i) {
 			keys2[i] = keys_i[i % key_len];
 		}
 		char input2[str_len * 5 + 5];
 		sprintf(input2, "%s?%s?%s?%s?%s", input, input, input, input, input);
-		hcreate(UINT_MAX>>11);
 		uint64_t count = Solve(str_len, input, key_len, keys_i);
 		sum += count;
 		str_len = strlen(input2);
 		count = Solve(str_len, input2, key_len * 5, keys2);
-		hdestroy();
 		sum2 += count;
 
 	}
@@ -57,7 +59,7 @@ void Day12() {
 	printf("%lu\n", sum2);
 }
 
-uint64_t Solve(int str_len, char str[static str_len], int key_len, int keys[static key_len]) {
+uint64_t Solve(int str_len, char str[static str_len], int key_len, char keys[static key_len]) {
 	// no more string left
 	if (str_len == 0) {
 		// no more keys so we are finished
@@ -77,44 +79,22 @@ uint64_t Solve(int str_len, char str[static str_len], int key_len, int keys[stat
 			return FoundHash(str_len, str, key_len, keys);
 		case '?': {
 			// try the number of options we get from assuming a . and a #
-			ENTRY item;
-//			char key[512] = {};
-			char *key = malloc(sizeof(char)*(str_len+key_len*4 + 1));
-			char *where = key;
-			size_t printed = sprintf(where, "%s", str);
-			where+=printed;
-			for (int i = 0; i < key_len; ++i) {
-				printed = sprintf(where, "%d,",keys[i]);
-				where+=printed;
+			char *key = malloc(str_len + key_len);
+			strncpy(key, str, str_len);
+			memcpy(key+str_len, keys, key_len);
+			uint64_t data;
+			bool got = hashmap_get(m, key,str_len + key_len , &data);
+			if(!got){
+				data = FoundHash(str_len, str, key_len, keys);
+				hashmap_set(m, key, str_len+key_len, data);
 			}
-			uint64_t data = FoundHash(str_len, str, key_len, keys);
-			item.data = (void *) data;
-			item.key = key;
-			hsearch(item, ENTER);
 			return Solve(str_len - 1, str + 1, key_len, keys) + data;
 		}
 	}
 	return 0;
 }
-uint64_t FoundHash(int str_len, char str[static str_len], int key_len, int keys[static key_len]) {
+uint64_t FoundHash(int str_len, char str[static str_len], int key_len, char keys[static key_len]) {
 	//no more keys left, no more options
-	ENTRY *found;
-	ENTRY item;
-//	char *key = malloc(sizeof(char)*(str_len+key_len*4 + 1));
-	char key[512] = {};
-	char *where = key;
-	size_t printed = sprintf(where, "%s", str);
-	where+=printed;
-	for (int i = 0; i < key_len; ++i) {
-		printed = sprintf(where, "%d,",keys[i]);
-		where += printed;
-	}
-	item.key = key;
-	found = hsearch(item, FIND);
-	if(found != NULL){
-//		free(found->key);
-		return (uint64_t)found->data;
-	}
 	if (key_len == 0) {
 		return 0;
 	}
