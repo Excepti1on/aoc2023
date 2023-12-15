963983f3ea849f8dfa94d4911249e6e47aa1f73c
Oliver Woelbing <oliver.woelbing@gmail.com>
2023-12-15 20:55:54 +0100

    format

--- day15/d15.c
+++ day15/d15.c
@@ -5,8 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct ListNode
-{
+typedef struct ListNode {
     int value;
     int name;
     struct ListNode *next;
@@ -16,12 +15,11 @@ static void Insert(ListNode **head, int value, int name);
 
 static void Delete(ListNode **head, int name);
 
-static int SumMap(ListNode *hashmap[256]);
+static int SumMap(ListNode *hashmap[static 256]);
 
-static void DeleteMap(ListNode *hashmap[256]);
+static void DeleteMap(ListNode *hashmap[static 256]);
 
-void Day15()
-{
+void Day15() {
     FILE *file = fopen("../day15/input.txt", "r");
     char buffer[32768] = {};
     int sum = 0;
@@ -29,12 +27,10 @@ void Day15()
     fgets(buffer, sizeof buffer, file);
     char *end;
     char *p = strtok(buffer, ",");
-    while (p)
-    {
+    while (p) {
         int value = 0;
         size_t i = 0;
-        while (p[i] != '\0')
-        {
+        while (p[i] != '\0') {
             value += p[i];
             value *= 17;
             value %= 256;
@@ -47,19 +43,15 @@ void Day15()
         int num = strtol(val + 1, NULL, 10);
         value = 0;
         i = 0;
-        while (isalpha(p[i]))
-        {
+        while (isalpha(p[i])) {
             value += p[i];
             value *= 17;
             value %= 256;
             i++;
         }
-        if (num != 0)
-        {
+        if (num != 0) {
             Insert(&hashmap[value], num, name);
-        }
-        else
-        {
+        } else {
             Delete(&hashmap[value], name);
         }
         p = strtok(NULL, "\n,");
@@ -69,29 +61,24 @@ void Day15()
     DeleteMap(hashmap);
 }
 
-void Insert(ListNode **head, int value, int name)
-{
+void Insert(ListNode **head, int value, int name) {
     ListNode *node = (ListNode *)malloc(sizeof(ListNode));
     node->next = NULL;
     node->value = value;
     node->name = name;
     ListNode *temp = *head;
-    if (temp == NULL)
-    {
+    if (temp == NULL) {
         *head = node;
         return;
     }
-    if (temp->name == name)
-    {
+    if (temp->name == name) {
         temp->value = value;
         free(node);
         return;
     }
-    while (temp->next != NULL)
-    {
+    while (temp->next != NULL) {
         temp = temp->next;
-        if (temp->name == name)
-        {
+        if (temp->name == name) {
             temp->value = value;
             free(node);
             return;
@@ -101,25 +88,20 @@ void Insert(ListNode **head, int value, int name)
     temp->next = node;
 }
 
-void Delete(ListNode **head, int name)
-{
-    if (*head == NULL)
-    {
+void Delete(ListNode **head, int name) {
+    if (*head == NULL) {
         return;
     }
     ListNode *temp = *head;
-    if ((*head)->name == name)
-    {
+    if ((*head)->name == name) {
         *head = (*head)->next;
         free(temp);
         return;
     }
-    while (temp->next != NULL && temp->next->name != name)
-    {
+    while (temp->next != NULL && temp->next->name != name) {
         temp = temp->next;
     }
-    if (temp->next == NULL)
-    {
+    if (temp->next == NULL) {
         return;
     }
     ListNode *delete = temp->next;
@@ -127,15 +109,12 @@ void Delete(ListNode **head, int name)
     free(delete);
 }
 
-int SumMap(ListNode *hashmap[256])
-{
+int SumMap(ListNode *hashmap[static 256]) {
     int sum = 0;
-    for (size_t i = 0; i < 256; i++)
-    {
+    for (size_t i = 0; i < 256; i++) {
         ListNode *temp = hashmap[i];
         int index = 1;
-        while (temp != NULL)
-        {
+        while (temp != NULL) {
             sum += (i + 1) * index * temp->value;
             temp = temp->next;
             index++;
@@ -144,14 +123,11 @@ int SumMap(ListNode *hashmap[256])
     return sum;
 }
 
-void DeleteMap(ListNode *hashmap[256])
-{
-    for (size_t i = 0; i < 256; i++)
-    {
+void DeleteMap(ListNode *hashmap[static 256]) {
+    for (size_t i = 0; i < 256; i++) {
         ListNode *temp = hashmap[i];
         ListNode *delete = temp;
-        while (temp != NULL)
-        {
+        while (temp != NULL) {
             temp = temp->next;
             free(delete);
             delete = temp;
